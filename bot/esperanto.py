1c323785816edce815604991b15a401840a62730
espero451 <253462134+espero451@users.noreply.github.com>
2026-01-28 19:21:41 +0100

    refactor: replace consecutive if with elif

--- bot/esperanto.py
+++ bot/esperanto.py
@@ -5,13 +5,13 @@ def esperanto(vorto):
     if vorto.endswith(suffixes):
         vorto = vorto[:-2] + "i"
 
-    if vorto.endswith('jn'):
-        vorto = vorto[:-2]
-
-    if vorto.endswith('j') and vorto not in {'kaj'}:
+    elif vorto.endswith('j') and vorto not in {'kaj'}:
         vorto = vorto[:-1]
 
-    if vorto.endswith('n') and vorto not in {'kun', 'sen', 'en', 'nun'}:
+    elif vorto.endswith('jn'):
+        vorto = vorto[:-2]
+
+    elif vorto.endswith('n') and vorto not in {'kun', 'sen', 'en', 'nun'}:
         vorto = vorto[:-1]
 
     vorto = vorto.replace("cx", "ĉ").replace("sx", "ŝ").replace("gx", "ĝ").replace("hx", "ĥ").replace("ux", "ŭ").replace("jx", "ĵ")
